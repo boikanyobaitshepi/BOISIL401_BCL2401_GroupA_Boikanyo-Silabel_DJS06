efea9b13064ef88dfec5cbb7b0d404ca06a84f3e
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-05-24 08:19:32 +0200

    logged each product name

--- scripts.js
+++ scripts.js
@@ -1,34 +1,43 @@
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
 
-names.forEach((name) => console.log(name))
 
-let index = 0
-names.forEach((name) => {
-console.log(`${name} (${provinces[index]})`)
-index = index + 1
- })
+// forEach Basics
+provinces.forEach(province => console.log(province));
+names.forEach(name => console.log(name));
+names.forEach((name, index) => console.log(`${name} (${provinces[index]})`) );
 
-const provincesUpperCase = provinces.map((province) => province.toUpperCase())
-console.log(provincesUpperCase)
+// Uppercase Transformation
+const uppercaseProvinces = provinces.map(province => province.toUpperCase());
+console.log(uppercaseProvinces);
 
+// Name Length
 const nameLength = names.map((name) => name.length)
 console.log(nameLength)
 
-console.log(provinces.sort())
+//Sorting
+const sortedProvinces = provinces.slice().sort(); // Slice to avoid mutating the original
+console.log(sortedProvinces);
 
-const filterCape = provinces.filter((province) => !province.includes('Cape'))
-console.log(filterCape)
+//  Filtering Cape
+const noCapeProvinces = provinces.filter(province => !province.includes('Cape'));
+console.log(`noCapeProvinces: ${noCapeProvinces.length}`);
 
-const findingS = names.map((name) => {
- return name.split('').some((s) => s.includes('S'))
-})
 
-console.log(findingS)
+// Finding 'S'
+const namesWithS = names.map(name => name.toLowerCase().includes('s'));
+console.log(namesWithS);
 
-console.log(names[0].split(''))
+const products = [
+  { product: 'banana', price: "2" },
+  { product: 'mango', price: 6 },
+  { product: 'potato', price: ' ' },
+  { product: 'avocado', price: "8" },
+  { product: 'coffee', price: 10 },
+  { product: 'tea', price: '' },
+];
 
-const createObjectMapping = names.reduce((acc, name, index) => {
-acc[name] = provinces[index]
- return acc
-}, {})
-
-console.log(createObjectMapping)
+// Log each product name
+products.forEach(product => {
+  console.log(product.name);
+});
